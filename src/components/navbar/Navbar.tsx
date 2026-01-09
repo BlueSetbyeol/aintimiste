88206eca78b1646320fc475971d2109779e6aaf7
Lisa Sauvinet <sauvinetlisa@gmail.com>
2026-01-09 14:43:00 +0100

    feat: navbar finalisation (#1)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,21 @@
 import "./Navbar.css"
 import logo from '../../assets/img/logo-horizontale.svg';
+import { Link } from "react-router-dom";
+
 
 export default function Navbar() {
     return (
         <nav className="navbar">
             <div className="navbar-left">
-                <img src={logo} alt="Aintimiste" />
+                <Link to ="/">
+                    <img src={logo} alt="Aintimiste" />
+                </Link>
             </div>
             <div className="navbar-right">
                 <a href="#reserver">RÉSERVER UNE TABLE</a>
-                <a href="#offrir">PRODUCTEUR & TERRITOIRE</a>
+                <Link to ="/producteurs-territoire">
+                    PRODUCTEURS & TERRITOIRES
+                </Link>
                 <a href="#carte">CARTE & MENUS</a>
             </div>
         </nav> )
