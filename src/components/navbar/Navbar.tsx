9ef712fc1da60a7010d6542182759992392e3276
Lisa Sauvinet <sauvinetlisa@gmail.com>
2026-01-09 14:35:18 +0100

    feat: add navbar (#1)

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,16 @@
+import "./Navbar.css"
+import logo from '../../assets/img/logo-horizontale.svg';
+
 export default function Navbar() {
-    return <nav>
-        <p>Hi</p>
-    </nav>
+    return (
+        <nav className="navbar">
+            <div className="navbar-left">
+                <img src={logo} alt="Aintimiste" />
+            </div>
+            <div className="navbar-right">
+                <a href="#reserver">RÉSERVER UNE TABLE</a>
+                <a href="#offrir">PRODUCTEUR & TERRITOIRE</a>
+                <a href="#carte">CARTE & MENUS</a>
+            </div>
+        </nav> )
 }
--- src/page/Producteurs/Producteurs.tsx
+++ src/page/Producteurs/Producteurs.tsx
@@ -1,3 +1,6 @@
+import Navbar from "../../components/navbar/Navbar";
+
 export default function Producteurs() {
-    return <></>
+    return <>
+    </>
 }
