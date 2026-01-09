128f4efc7eae2dc6d45f767f1db7a287d3c5c840
BlueSetbyeol <setbyeol.c@gmail.com>
2026-01-09 16:07:03 +0100

    feat : text & image component incomming (#3)

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,8 +1,28 @@
+import ImageText from "../../components/ImageText/ImageText";
+import TextImageInfo from "../../assets/text";
+
 export default function Home() {
   return (
     <>
       <section>
-        <p>Soon to be done</p>
+        <ImageText
+          image={TextImageInfo[1].image.url}
+          titre={TextImageInfo[1].titre}
+          texte1={TextImageInfo[1].texte1}
+          texte2={TextImageInfo[1].texte2}
+          lien1={TextImageInfo[1].lien1}
+          lien2={TextImageInfo[1].lien2}
+          imageSide={"right"}
+        />
+        <ImageText
+          image={TextImageInfo[0].image.url}
+          titre={TextImageInfo[0].titre}
+          texte1={TextImageInfo[0].texte1}
+          texte2={TextImageInfo[0].texte2}
+          lien1={TextImageInfo[0].lien1}
+          lien2={TextImageInfo[0].lien2}
+          imageSide={"left"}
+        />
       </section>
     </>
   );
